cd5eb8148a22e6ec2bb4a197564e1770270c3487
Kiran <send2kiran.n@gmail.com>
2025-06-26 23:08:02 +1000

    clean.

--- Anderson.Console/obj/Debug/net9.0/Anderson.Console.AssemblyInfo.cs
+++ Anderson.Console/obj/Debug/net9.0/Anderson.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anderson.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5250dbb145ed9945362e30fbe07cb5b0805283b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b189230b3603dfc536d73702c6dc669035140c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anderson.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anderson.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
