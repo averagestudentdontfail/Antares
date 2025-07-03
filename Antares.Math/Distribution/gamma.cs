d94a226552635c9ad7774e16161d9a6aa9462d3d
Kiran <send2kiran.n@gmail.com>
2025-07-03 13:59:53 +1000

    curren system, fixing malformations in the code comments and documentation.

--- Antares.Math/Distribution/gamma.cs
+++ Antares.Math/Distribution/gamma.cs
@@ -25,7 +25,7 @@ namespace Antares.Math.Distribution
         public CumulativeGammaDistribution(double a)
         {
             if (a <= 0.0)
-                throw new ArgumentOutOfRangeException(nameof(a), "Shape parameter 'a' must be positive.");
+                throw new ArgumentException("Invalid parameter for gamma distribution. 'a' must be positive.", nameof(a));
             _a = a;
         }
 
@@ -33,12 +33,13 @@ namespace Antares.Math.Distribution
         /// Calculates the cumulative distribution function (CDF) at a given point x.
         /// </summary>
         /// <param name="x">The value at which to evaluate the CDF.</param>
-        /// <returns>The probability P(X <= x) for a Gamma(a, 1.0) random variable X.</returns>
+        /// <returns>The probability P(X &lt;= x) for a Gamma(a, 1.0) random variable X.</returns>
         public double Value(double x)
         {
             if (x <= 0.0)
                 return 0.0;
-            // Use MathNet.Numerics for the CDF
+
+            // The standard gamma distribution has a rate (beta) of 1.0.
             return Gamma.CDF(_a, 1.0, x);
         }
     }
@@ -47,36 +48,32 @@ namespace Antares.Math.Distribution
     /// Gamma function class.
     /// </summary>
     /// <remarks>
-    /// This is a function defined by Γ(z) = ∫[0,∞] t^(z-1)e^(-t)dt.
+    /// <para>
+    /// The gamma function is defined as Gamma(z) = integral from 0 to infinity of t^(z-1)*e^(-t) dt.
+    /// </para>
+    /// <para>
+    /// The implementation of the algorithm from the original C++ code (a Lanczos
+    /// approximation) has been replaced by calls to the highly optimized special
+    /// functions in the MathNet.Numerics library.
+    /// </para>
     /// </remarks>
     public class GammaFunction
     {
         /// <summary>
-        /// Initializes a new instance of the GammaFunction class.
-        /// </summary>
-        /// <remarks>
-        /// This class is provided as a placeholder to match the original interface.
-        /// The actual implementation uses MathNet.Numerics.SpecialFunctions.
-        /// </remarks>
-        public GammaFunction()
-        {
-        }
-
-        /// <summary>
-        /// Calculates the value of the Gamma function, Γ(x).
+        /// Calculates the value of the Gamma function, Gamma(x).
         /// </summary>
         /// <param name="x">The input value.</param>
-        /// <returns>The value of Γ(x).</returns>
+        /// <returns>The value of Gamma(x).</returns>
         public double Value(double x)
         {
             return SpecialFunctions.Gamma(x);
         }
 
         /// <summary>
-        /// Calculates the natural logarithm of the Gamma function, ln(Γ(x)).
+        /// Calculates the natural logarithm of the Gamma function, ln(Gamma(x)).
         /// </summary>
         /// <param name="x">The input value. Must be positive.</param>
-        /// <returns>The value of ln(Γ(x)).</returns>
+        /// <returns>The value of ln(Gamma(x)).</returns>
         public double LogValue(double x)
         {
             if (x <= 0.0)
